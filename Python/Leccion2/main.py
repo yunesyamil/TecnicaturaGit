d433366b08f15a3523f3d52557ad0dc68a67218d
Yamil Yunes <yunesyamil@gmail.com>
2023-06-27 22:04:00 -0300

    Ingresamos el pryecto de Python al repositorio

---
+++ Python/Leccion2/main.py
@@ -0,0 +1,9 @@
+# Sentencia if / else
+
+condicion = 10
+if condicion == True:
+    print('La condición es Verdadera')
+elif condicion == False:
+    print('la condición es Falsa')
+else:
+    print('Condición sin especificar')
