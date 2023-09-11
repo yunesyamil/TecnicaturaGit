4600445a6f24b3336ef9bc36af8d85ae6d0c6134
Yamil Yunes <yunesyamil@gmail.com>
2023-09-11 18:14:33 -0300

    Reorganización de carpetas

--- Java/Leccion2/EjercicioCiclos02/src/Ciclos02/Ciclos02.java
+++ Java/Leccion2/EjercicioCiclos02/src/Ciclos02/Ciclos02.java
@@ -9,25 +9,22 @@ package Ciclos02;
 
 import javax.swing.JOptionPane;
 
-
 /**
  * @author Yamil
  */
 public class Ciclos02 {
+
     public static void main(String[] args) {
-        
+
         var numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
-        while(numero != 0){
-            if(numero > 0){
-                JOptionPane.showMessageDialog(null, "El numero "+numero+" es POSITIVO.");
+        while (numero != 0) {
+            if (numero > 0) {
+                JOptionPane.showMessageDialog(null, "El numero " + numero + " es POSITIVO.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El numero " + numero + " es NEGATIVO.");
             }
-            else{
-                JOptionPane.showMessageDialog(null, "El numero "+numero+" es NEGATIVO.");
-            }
-            
             numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero: "));
         }
-        JOptionPane.showMessageDialog(null, "El numero "+numero+" finaliza el programa.");
-        
+        JOptionPane.showMessageDialog(null, "El numero " + numero + " finaliza el programa.");
     }
 }
