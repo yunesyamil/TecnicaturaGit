951d088932359962372edcc14320ecfa940f0787
Yamil Yunes <yunesyamil@gmail.com>
2023-10-16 12:25:38 -0300

    Java Clase 5 Clases y Objetos Parte 3

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -11,4 +11,20 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " +resultado);
     }
+    // 2 Creamos otro metodo
+        
+    public int sumarConRetorno(){
+        int resultado = a + b ;
+        return resultado;
+    }
+    
+    //3 Creamos otro metodo
+    public int sumarConArgumentos(int arg1, int arg2){   
+        this.a = arg1; //El argumento a se aisgna al tributo this.a
+        this.b = arg2; 
+        // return a + b;
+        return this.sumarConRetorno(); //Dentro del metodo retornamos otro metodo
+    }
 }
+
+//No se recomienda crear el metodo main aqui
