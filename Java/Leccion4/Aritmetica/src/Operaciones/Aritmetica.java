9f562ff8c5ed4c728b852ee93153d8cada945eb9
Yamil Yunes <yunesyamil@gmail.com>
2023-10-16 11:33:19 -0300

    Java Clase 3 y 4 Ciclos Clases Objetos

---
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+
+package Operaciones;
+
+public class Aritmetica {
+    // Atributos de la clase
+    int a; //El valor por default es 0 si no se inicializa.
+    int b;
+    
+    //Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " +resultado);
+    }
+}
